26d0024a820b4bbc965d37ea607ec57d77385048
Jeff Meyer <jcmeyer10@gmail.com>
2015-11-09 11:26:37 -0500

    "modifies schema for beer and locations"

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806195336) do
+ActiveRecord::Schema.define(version: 20151109162051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "brewery"
+    t.string "type"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "isbn"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "street_num"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
