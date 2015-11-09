c729aadc411bcc4785ed60b14c63693f210b5577
Jeff Meyer <jcmeyer10@gmail.com>
2015-11-09 12:09:53 -0500

    adds controllers to beers and locations

---
+++ app/controllers/beers_controller.rb
@@ -0,0 +1,54 @@
+class BeersController < ApplicationController
+before_action :set_beer, only: [:update, :destroy]
+
+  # GET /beers
+  def index
+    @beers = Beer.all
+
+    render json: @beers
+  end
+
+  # GET /beers/1
+  def show
+    @beer = Beer.find(params[:id])
+
+    render json: @beer
+  end
+
+  # POST /beers
+  def create
+    @beer = current_user.beers.new(beer_params)
+
+    if @beer.save
+      render json: @beer, status: :created, beer: @beer
+    else
+      render json: @beer.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /beers/1
+  def update
+    if @beer.update(beer_params)
+      head :no_content
+    else
+      render json: @beer.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /beers/1
+  def destroy
+    @beer.destroy
+
+    head :no_content
+  end
+
+  def set_beer
+    @beer = current_user.beers.find(params[:id])
+  end
+
+  def beer_params
+    params.require(:beer).permit(:name, :brewery, :type)
+  end
+
+  private :set_beer, :beer_params
+end
