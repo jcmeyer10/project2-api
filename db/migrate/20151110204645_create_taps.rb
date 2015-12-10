62c2273a5f558b8b3232605c24d90fc91f0db1a2
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-10 16:23:55 -0500

    working through heroku deployment errors

--- db/migrate/20151110204645_create_taps.rb
+++
@@ -1,8 +0,0 @@
-class CreateTaps < ActiveRecord::Migration
-  def change
-    create_table :taps do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20151111194606_add_location_ref_to_taps.rb
+++
@@ -1,5 +0,0 @@
-class AddLocationRefToTaps < ActiveRecord::Migration
-  def change
-    add_reference :taps, :location, index: true, foreign_key: true
-  end
-end
